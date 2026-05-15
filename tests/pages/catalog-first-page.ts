b4a749f1b5b5f972b0cb565b2578186ac8e4a8c1
AnnA <a.popovych@macpaw.com>
2026-05-15 17:06:27 +0100

    fix Prettier error

--- tests/pages/catalog-first-page.ts
+++ tests/pages/catalog-first-page.ts
@@ -1,18 +1,17 @@
-import {BasePage} from "./base-page";
-import {Locator} from "@playwright/test";
+import { BasePage } from './base-page';
+import { Locator } from '@playwright/test';
 
-export class CatalogFirstPage extends BasePage{
+export class CatalogFirstPage extends BasePage {
+  get pageLayout() {
+    return this.page.locator('.layout .portal__heading');
+  }
 
-    get pageLayout() {
-        return this.page.locator('.layout .portal__heading');
-    }
+  get categoryTiles(): Locator {
+    return this.page.locator('.tile-cats__heading');
+  }
 
-    get categoryTiles(): Locator {
-        return this.page.locator('.tile-cats__heading');
-    }
-
-    async openSubCategoryByName(subCategoryName: string) {
-        const targetCategory = this.categoryTiles.filter({ hasText: subCategoryName });
-        await targetCategory.click();
-    }
+  async openSubCategoryByName(subCategoryName: string) {
+    const targetCategory = this.categoryTiles.filter({ hasText: subCategoryName });
+    await targetCategory.click();
+  }
 }
