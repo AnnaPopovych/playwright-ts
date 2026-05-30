78d366fd28f4eef2795372bc496a9bd52919ae83
AnnA <a.popovych@macpaw.com>
2026-05-30 09:27:54 +0100

    added changes in playwright file and gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,17 +5,28 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  workers: 2,
   reporter: 'html',
   use: {
+    baseURL: 'https://rozetka.com.ua/',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-  },
-  projects: [
-    {
-      name: 'mobile-safari',
-      testMatch: /.*\.catalog\.spec\.ts/,
-      use: { ...devices['iPhone 12'] },
+    video: {
+      mode: 'on-first-retry',
+      size: { width: 640, height: 480 },
+      show: {
+        actions: {
+          duration: 500,
+          position: 'top-right',
+          fontSize: 14,
+        },
+      },
     },
+    actionTimeout: 10 * 1000,
+    navigationTimeout: 30 * 1000,
+  },
+  expect: { timeout: 30_000 },
+  projects: [
     {
       name: 'smoke-chromium',
       testMatch: /.*\.smoke\.spec\.ts/,
