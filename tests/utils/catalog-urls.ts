5b0a8140692668206edb3a3dd5a3996303c5c17f
AnnA <a.popovych@macpaw.com>
2026-05-30 09:34:18 +0100

    changed in catalog-urls

--- tests/utils/catalog-urls.ts
+++ tests/utils/catalog-urls.ts
@@ -1,5 +1,5 @@
 export const URLS = {
-  HOME: 'https://rozetka.com.ua/',
-  HOUSEHOLD_GOODS: 'https://rozetka.com.ua/ua/tovary-dlya-doma/c2394287/',
-  KITCHEN_UTENSILS: 'https://rozetka.com.ua/ua/posuda-dlia-kuhni/c4626594/',
+  HOME: '/',
+  HOUSEHOLD_GOODS: '/ua/tovary-dlya-doma/c2394287/',
+  KITCHEN_UTENSILS: '/ua/posuda-dlia-kuhni/c4626594/',
 };
