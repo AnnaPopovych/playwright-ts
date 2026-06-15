3a44f140339da1059e5871444f9b2bc0be68f72e
Hanna <39803990+AnnaPopovych@users.noreply.github.com>
2026-06-15 10:21:13 +0100

    Clean up main-page.ts by removing unused
    Removed unused getters and scroll function from main-page.ts.

--- tests/pages/main-page.ts
+++ tests/pages/main-page.ts
@@ -6,40 +6,8 @@ export class MainPage extends BasePage {
     return this.page.locator('[data-testid="fat_menu_category_link"]');
   }
 
-  get listBestForYou(): Locator {
-    return this.page.locator('.goods-sections-best.indent');
-  }
-
-  get moreButton(): Locator {
-    return this.page.locator('button.read-more-btn.button.button_size_medium.button_color_green');
-  }
-
   async selectCategoryByName(categoryName: string) {
     const target = this.sideMenuCategories.filter({ hasText: categoryName });
     await target.click();
-  }
-
-  async scrollUntilMoreButtonVisible() {
-    let previousHeight = 0;
-
-    while (true) {
-      if (await this.moreButton.isVisible()) {
-        return;
-      }
-
-      const currentHeight = await this.page.evaluate(() => document.body.scrollHeight);
-
-      if (currentHeight === previousHeight) {
-        break;
-      }
-
-      previousHeight = currentHeight;
-      await this.page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-      await this.page
-        .waitForFunction((h) => document.body.scrollHeight > h, previousHeight)
-        .catch(() => {});
-    }
-
-    await this.moreButton.scrollIntoViewIfNeeded();
   }
 }
